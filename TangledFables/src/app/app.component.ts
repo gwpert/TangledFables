4fc9d1b81e7f075e346cc5b09db01098bbbdcda2
Greg <gwpert@gmail.com>
2020-09-05 16:27:39 -0400

    Add a mock login authentication using firestore.

--- TangledFables/src/app/app.component.ts
+++ TangledFables/src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { User } from './services/user.model';
+import { AuthService } from './services/auth.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TangledFables';
+
+
+  
+  constructor(public auth: AuthService) {
+
+  };
+
 }
--- TangledFables/src/app/app.module.ts
+++ TangledFables/src/app/app.module.ts
@@ -5,6 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
+//Firebase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC3kSHXJQLyASdtU1HJVCbAtzDH6v9uwYQ",
+  authDomain: "tangledfables.firebaseapp.com",
+  databaseURL: "https://tangledfables.firebaseio.com",
+  projectId: "tangledfables",
+  storageBucket: "tangledfables.appspot.com",
+  messagingSenderId: "491533736911",
+  appId: "1:491533736911:web:a9adb7ba8ead757ce6d0ba",
+  measurementId: "G-87LQLCP4B3"
+};
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +30,11 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
